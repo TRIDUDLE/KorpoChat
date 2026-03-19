a743988182b654616ca8652fcf5d0ff20f0a43f2
MarcelCzekala <mc311484@student.polsl.pl>
2026-03-19 13:15:29 +0100

    Test

--- backend/src/main/java/com/korpochat/backend/BackendApplication.java
+++ backend/src/main/java/com/korpochat/backend/BackendApplication.java
@@ -3,6 +3,8 @@ package com.korpochat.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//test
 @SpringBootApplication
 public class BackendApplication {
 
