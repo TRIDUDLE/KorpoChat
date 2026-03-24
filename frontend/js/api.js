c5194cd278c71bab0996e1932ef42705cf742098
TRIDUDLE <b.slabik@protonmail.com>
2026-03-24 10:43:34 +0100

    Refactor login function to use API for authentication and improve error handling

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,95 +1,105 @@
-// frontend/js/api.js
+    // frontend/js/api.js
 
-export const api = {
-    BASE_URL: 'http://localhost:8080/api',
+    export const api = {
+        BASE_URL: 'http://localhost:8080/api',
 
-    login: async (username, password) => {
-        // Fetch current users from local storage MOCK database
-        const users = await api.getUsers();
+        login: async (username, password) => {
+            try {
+                //sending true HTTP POST request to Spring Boot server
+                const response = await fetch(`${api.BASE_URL}/auth/login`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ 
+                        username: username, 
+                        password: password 
+                    }) //login request body
+                });
 
-        // Search for a user matching BOTH username and password
-        const foundUser = users.find(u => u.username === username && u.password === password);
-
-        // Simulate network delay and return outcome
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (foundUser) {
-                    // G: Return mock token and the actual role of the found user
-                    resolve({ token: 'mock-jwt-token', role: foundUser.role });
-                } else {
-                    // fail: User not found or wrong password
-                    reject(new Error('Invalid credentials'));
+                // If response is not 200 OK, throw an error to be caught in app.js
+                if (!response.ok) {
+                    throw new Error('Invalid username or password');
                 }
-            }, 300);
-        });
-    },
 
-    getUsers: async () => {
-        //MOCK
-        const storedUsers = localStorage.getItem('users');
-        if (storedUsers) {
-            return JSON.parse(storedUsers);
-        }else {
-            const defaultUsers = [
-                { id: 1, username: 'admin',password:'admin', role: 'ADMIN', status: 'ONLINE', last_seen: 'Teraz' },
-                { id: 2, username: 'bartosz',password:'elza', role: 'USER', status: 'OFFLINE', last_seen: '15 min temu' },
-                { id: 3, username: 'marcel',password:'luna', role: 'USER', status: 'ONLINE', last_seen: 'Teraz' },
-            ];
-            localStorage.setItem('default-users', JSON.stringify(defaultUsers));
-            return defaultUsers;
-        }    
-    
-    },
-    addUser: async (username,password,role) => {
-        const users = await api.getUsers();
-        const newUser = {
-            id: users.length + 1, // Mock ID generation
-            username: username,
-            password: password, //plaintext for mock frontend
-            role: role,
-            status: 'OFFLINE', //default status for new users
-            last_seen: 'N/A'
-        };
-        users.push(newUser);
-        localStorage.setItem('users', JSON.stringify(users)); // Mock persistence
+                //if 200 OK, parse the JSON response
+                const data = await response.json();
+                
+                //app.js awaits object with "role" field
+                return data; 
 
-        return newUser;
-    },
-    getMessages: async () => {
-        //MOCK
-        const storedMessages= localStorage.getItem('chat_messages');
-        if (storedMessages){
-            return JSON.parse(storedMessages);
-        }else{
-            //default messge if emty
-            const defaultMessages=[
-                {
-                id: Date.now(),
-                sender: 'system',
-                text: 'to jest wiadomosc testowa!',
+            } catch (error) {
+                console.error("server connection error:", error);
+                throw error; 
+            }
+        },
+
+        getUsers: async () => {
+            //MOCK
+            const storedUsers = localStorage.getItem('users'); 
+            if (storedUsers) {
+                return JSON.parse(storedUsers);
+            }else {
+                const defaultUsers = [
+                    { id: 1, username: 'admin',password:'admin', role: 'ADMIN', status: 'ONLINE', last_seen: 'Teraz' },
+                    { id: 2, username: 'bartosz',password:'elza', role: 'USER', status: 'OFFLINE', last_seen: '15 min temu' },
+                    { id: 3, username: 'marcel',password:'luna', role: 'USER', status: 'ONLINE', last_seen: 'Teraz' },
+                ];
+                localStorage.setItem('users', JSON.stringify(defaultUsers));
+                return defaultUsers;
+            }    
+        
+        },
+        addUser: async (username,password,role) => {
+            const users = await api.getUsers();
+            const newUser = {
+                id: users.length + 1, // Mock ID generation
+                username: username,
+                password: password, //plaintext for mock frontend
+                role: role,
+                status: 'OFFLINE', //default status for new users
+                last_seen: 'N/A'
+            };
+            users.push(newUser);
+            localStorage.setItem('users', JSON.stringify(users)); // Mock persistence
+
+            return newUser;
+        },
+        getMessages: async () => {
+            //MOCK
+            const storedMessages= localStorage.getItem('chat_messages');
+            if (storedMessages){
+                return JSON.parse(storedMessages);
+            }else{
+                //default messge if emty
+                const defaultMessages=[
+                    {
+                    id: Date.now(),
+                    sender: 'system',
+                    text: 'to jest wiadomosc testowa',
+                    timestamp: new Date().toISOString()
+                    }
+                ];
+                localStorage.setItem('chat_messages', JSON.stringify(defaultMessages));
+                return defaultMessages;
+            }
+        },
+        sendMessage: async (sender, text) => {
+            // Retrieve existing messages
+            const messages = await api.getMessages();
+            
+            // Create new message object
+            const newMessage = {
+                id: Date.now(), // MOCK ID
+                sender: sender,
+                text: text,
                 timestamp: new Date().toISOString()
-                }
-            ];
-            localStorage.setItem('chat_messages', JSON.stringify(defaultMessages));
-            return defaultMessages;
+            };
+            
+            // Append and save to storage
+            messages.push(newMessage);
+            localStorage.setItem('chat_messages', JSON.stringify(messages));
+            
+            return newMessage;
         }
-    },
-    sendMessage: async (sender, text) => {
-        // Retrieve existing messages
-        const messages = await api.getMessages();
-        
-        // Create new message object
-        const newMessage = {
-            id: Date.now(), // MOCK ID
-            sender: sender,
-            text: text,
-            timestamp: new Date().toISOString()
-        };
-        
-        // Append and save to storage
-        messages.push(newMessage);
-        localStorage.setItem('chat_messages', JSON.stringify(messages));
-        
-        return newMessage;
-    }
-};
+    };
