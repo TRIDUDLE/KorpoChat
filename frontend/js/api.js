79c2e433d0a7e71a77c4665d35947295f7d64b41
TRIDUDLE <b.slabik@protonmail.com>
2026-03-24 11:21:41 +0100

    typo fixes in api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -61,7 +61,7 @@
                 const response = await fetch(`${api.BASE_URL}/users`, {
                     method: 'POST',
                     headers: {
-                        'Content-Type': 'application/json';
+                        'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
                         username: username,
